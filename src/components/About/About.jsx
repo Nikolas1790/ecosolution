b18291d64e52d2a7785b7339fd5df797089720a5
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-07-02 02:30:04 +0300

    about-address

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,14 @@
+import { AboutAddress, AboutAddressBlock, AboutSection } from "./About.styled";
+
 export const About = () => {
   return (
-    <div >
-About
-  </div>
+    <AboutSection >
+      <AboutAddressBlock>
+        <AboutAddress>79005, Ukraine, Lvivstreet. Shota Rustaveli, 7</AboutAddress>
+        <AboutAddress>office@ecosolution.com</AboutAddress>
+      </AboutAddressBlock>
+
+      About
+    </AboutSection>
   );
 };
---
+++ src/components/About/About.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const AboutSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  gap: 36px;
+  padding: 24px 0 36px 0;
+`;
+
+
+export const AboutAddressBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const AboutAddress = styled.p`
+  letter-spacing: -0.64px;
+`;
